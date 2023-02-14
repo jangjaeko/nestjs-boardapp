bae5cea624c4bc43b283076765349b4c8a5db6fa
jangjaeko <jangjaeko45@gmail.com>
2023-02-14 17:56:59 +0900

    feat createBoard post mathod

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { BoardsService } from './boards.service';
+import { Board } from './boards.model';
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
-  getAllBoard() {
+  @Get('/')
+  getAllBoard(): Board[] {
     return this.boardsService.getAllBoards();
+  }
+  @Post()
+  createBoard(
+    @Body('title') title: string,
+    @Body('description') discription: string,
+  ): Board {
+    return this.boardsService.createBoard(title, discription);
   }
 }
