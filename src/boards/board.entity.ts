f3ec2d32875c0134be735ea17b179f5c8e9eacdf
jangjaeko <jangjaeko45@gmail.com>
2023-02-20 16:00:55 +0900

    feat: connect postgresql ;

--- src/boards/board.entity.ts
+++ src/boards/board.entity.ts
@@ -1,10 +1,17 @@
-export class Board extends BaseEntity{
-    @Primary
-    id: number;
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BoardStatus } from './boards.model';
 
-    @Column
-    title : string;
-    
-    @column
-    description : string
+@Entity()
+export class Board extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  status: BoardStatus;
 }
